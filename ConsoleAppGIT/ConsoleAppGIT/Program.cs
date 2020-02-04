6e3926d22e3a50d218317ac7ca2b2040f6492c65
DEIVI DIAZ H <Deivifdiazhenao@outlook.com>
2020-02-04 16:21:03 -0500

    Program: Deivif commit from master

--- ConsoleAppGIT/ConsoleAppGIT/Program.cs
+++ ConsoleAppGIT/ConsoleAppGIT/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppGIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola cami");
         }
     }
 }
