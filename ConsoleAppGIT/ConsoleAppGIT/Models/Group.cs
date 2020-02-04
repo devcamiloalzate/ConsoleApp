c5961a1148dc2704fb230c3e31e7cf644c07f9c8
DEIVI DIAZ H <Deivifdiazhenao@outlook.com>
2020-02-04 16:47:59 -0500

    Add Group model

---
+++ ConsoleAppGIT/ConsoleAppGIT/Models/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGIT.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+    }
+}
