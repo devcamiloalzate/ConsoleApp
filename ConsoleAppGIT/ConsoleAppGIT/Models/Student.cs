b0dc0043124e349ef8849a7d63ee52376003934d
DEIVI DIAZ H <Deivifdiazhenao@outlook.com>
2020-02-04 16:38:03 -0500

    AddStudentModel from RamaDeivif1

---
+++ ConsoleAppGIT/ConsoleAppGIT/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGIT.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
