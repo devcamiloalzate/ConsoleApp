803e3629897ead32b75fd4e96a1bd22a5b372e3e
camilo alzate uzma <dev.camiloalzate304@gmail.com>
2020-02-04 16:45:10 -0500

    Add Course model

---
+++ ConsoleAppGIT/ConsoleAppGIT/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGIT.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
