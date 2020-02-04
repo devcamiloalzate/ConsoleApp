8e8f9bff273358b775ee88a6214cfa49fbbdc5ba
camilo alzate uzma <dev.camiloalzate304@gmail.com>
2020-02-04 16:10:03 -0500

    Initial Commit

---
+++ ConsoleAppGIT/ConsoleAppGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
