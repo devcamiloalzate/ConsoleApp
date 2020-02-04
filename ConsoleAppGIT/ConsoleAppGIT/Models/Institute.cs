086e50613087f1347b7af79d1a69c20ca303a567
DEIVI DIAZ H <Deivifdiazhenao@outlook.com>
2020-02-04 17:11:13 -0500

    AddInstitute

---
+++ ConsoleAppGIT/ConsoleAppGIT/Models/Institute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppGIT.Models
+{
+    class Institute
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+    }
+}
