37ee0fbe698ab5d0c8c5dd6d2544e008daa17c7a
DEIVI DIAZ H <Deivifdiazhenao@outlook.com>
2020-02-05 10:59:20 -0500

    Program: Commit in master

--- ConsoleAppGIT/ConsoleAppGIT/Program.cs
+++ ConsoleAppGIT/ConsoleAppGIT/Program.cs
@@ -36,6 +36,7 @@ namespace ConsoleAppGIT
             Console.WriteLine("Nuevo commit en la master 16");
             Console.WriteLine("Nuevo commit en la master 17");
             Console.WriteLine("Nuevo commit en la master 18");
+            Console.WriteLine("Nuevo commit en la master 19");
         }
     }
 }
