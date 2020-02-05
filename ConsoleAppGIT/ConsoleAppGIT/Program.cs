9f9ed0286eecf45c08eaa67698536824d018b2c1
camilo alzate uzma <dev.camiloalzate304@gmail.com>
2020-02-05 10:48:34 -0500

    Program: Add another 2 WriteLine

--- ConsoleAppGIT/ConsoleAppGIT/Program.cs
+++ ConsoleAppGIT/ConsoleAppGIT/Program.cs
@@ -28,6 +28,8 @@ namespace ConsoleAppGIT
             Console.WriteLine("Nuevo commit en la master 8");
             Console.WriteLine("Nuevo commit en la master 9");
             Console.WriteLine("Nuevo commit en la master 10");
+            Console.WriteLine("Nuevo commit en la master 10");
+            Console.WriteLine("Nuevo commit en la master 10");
         }
     }
 }
